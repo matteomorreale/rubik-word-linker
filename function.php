f8a97fa390e5681f5174cc3a385c7af409f15744
Matteo Morreale <matteomorreale@users.noreply.github.com>
2024-05-22 15:52:35 +0200

    Add files via upload
    - Embed not broken
    - Words already linked are skipped
    - Titles not broken
    Many bug fix

--- function.php
+++ function.php
@@ -1,14 +1,14 @@
 <?php
-	/*
-	Plugin Name: Rubik Words Autolinker
-	Version: 1.0
-	Description: This plugin automatically creates links on certain words if they have been associated with a certain post
-	Author: Matteo Morreale
-	Author URI: https://matteomorreale.it
-	*/
+/*
+Plugin Name: Rubik Words Autolinker
+Version: 1.0
+Description: This plugin automatically creates links on certain words if they have been associated with a certain post
+Author: Matteo Morreale
+Author URI: https://matteomorreale.it
+*/
 
-	// Matteo Morreale - CC Attribuzione non Commerciale
-	// Hai libertà di riutilizzare, modificare e condividere questo sorgente ma non a fini commerciali, ed è necessario citarne l'autore
+// Matteo Morreale - CC Attribuzione non Commerciale
+// Hai libertà di riutilizzare, modificare e condividere questo sorgente ma non a fini commerciali, ed è necessario citarne l'autore
 
 /**
  * This plugin creates automatic links in the posts according to the keywords that have been indicated in the various posts.
@@ -20,24 +20,11 @@ require_once("inc/db-handler.php");
 require_once("inc/saving-catcher.php");
 require_once("structured-data.php");
 
-/** Hooks  
- *  Deactivated since we're using a query on posts, but may be usefull in the future with too many posts
-register_activation_hook( __FILE__, 'wdlnk_activation' );
-function wdlnk_activation(){
-	global $wpdb;
-	$table_name = $wpdb->prefix . "wdlnk";
-	
-	// If there's no table, we'll create one
-	if($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-		// Initializing the DB
-		$db_handler = new DbHandler();
-		$db_handler->db_install();
-	}
-}
-*/
-
 /* Replace all words */
 function replace_text_wps($text){
+    global $linked_entities; // Array per memorizzare le entità collegate
+    $linked_entities = array();
+
     if(function_exists("get_field") && !get_field("is_weekend_flag") ):
         // If there's a transiet set, than we take this instead of doin' the query again
         if(!empty(get_transient("wdlnk_replace_array"))){
@@ -45,20 +32,20 @@ function replace_text_wps($text){
         }
         else{
             // Searching for all main keywords in posts
-			$terms = get_terms( 'category', array(
-				'hide_empty' => true,
-			) );
+            $terms = get_terms( 'category', array(
+                'hide_empty' => true,
+            ));
 
-			$categories_id_list = array();
-			foreach($terms as $term){
-				$categories_id_list[] = $term->term_id;
-			}
+            $categories_id_list = array();
+            foreach($terms as $term){
+                $categories_id_list[] = $term->term_id;
+            }
 
             // Searching for all main keywords in posts
             $args = array(
                 'posts_per_page' => -1,
-                'post_type' => array("post"),
-				'category__in' => $categories_id_list,
+			    'post_type' => array("post"),
+                'category__in' => $categories_id_list,
                 'meta_key' => 'main_keyword',
                 'meta_value' => array(''),
                 'meta_compare' => 'NOT IN'
@@ -73,62 +60,87 @@ function replace_text_wps($text){
                 $replace[mb_strtolower ($post_main_word)] = get_the_permalink($p->ID);
             }
 			
-            // Searching for all main keywords in points and pages
+            // Searching for all main keywords in posts of type "point"
             $args = array(
                 'posts_per_page' => -1,
-                'post_type' => array("point","page"),
+                'post_type' => 'point',
                 'meta_key' => 'main_keyword',
                 'meta_value' => array(''),
                 'meta_compare' => 'NOT IN'
             );
             $meta_query = new WP_Query( $args );
-    
+
             // Adding every word to the array of words to replace with the link to the post
             foreach($meta_query->posts as $p){
                 $post_main_word = trim(get_field("main_keyword",$p->ID));
-                //$replace[mb_strtolower ($post_main_word)] = '<a href="' . get_the_permalink($p->ID) . '">'.$post_main_word.'</a>';
-				$replace[mb_strtolower ($post_main_word)] = get_the_permalink($p->ID);
-            }
-            
-            // Getting all entity Titles
-            $args = array(
-                'posts_per_page' => -1,
-                'post_type' => "point",
-				'post__not_in' => array( $post->ID )
-            );
-            $meta_query = new WP_Query( $args );
-
-            // Adding every title to the array of words to replace with the link to the post
-            foreach($meta_query->posts as $p){
-                $post_main_word = trim(get_the_title($p->ID));
                 $replace[mb_strtolower ($post_main_word)] = get_the_permalink($p->ID);
             }
-    
+
             // Setting the transient for future use
             set_transient("wdlnk_replace_array",json_encode($replace),172800); // Expires within two days
         }
 		
-		// Removing current post from the list, otherwise it'll link to himself
+		// Tolgo dalla lista il post corrente
 		unset($replace[mb_strtolower(trim(get_the_title(get_the_ID())))]);
-		unset($replace[mb_strtolower(trim(get_field("main_keyword",get_the_ID())))]);
-		
-        // Replacing
+		if(!empty(get_field("main_keyword",get_the_ID()))){
+				unset($replace[mb_strtolower(trim(get_field("main_keyword",get_the_ID())))]);	
+		}
+        // Replace words with links and store the linked entities
+        foreach($replace as $word => $link) {
+            $text = preg_replace_callback("/\\b$word\\b|(<a .*?<\\/a>)|(<img .*?>)/si", function($matches) use ($word, $link, &$linked_entities) {
+                if (strpos($matches[0], '<a') === 0 || strpos($matches[0], '<img') === 0) {
+                    return $matches[0];
+                } else {
+                    $linked_entities[$word] = $link; // Memorizza la parola chiave e il link
+                    return '<a href="' . $link . '" >' . ucwords($word) . '</a>';
+                }
+            }, $text);
+        }
 
-		foreach( $replace as $word => $link ){
-			$pattern = "/(?si)<a[^<]*>.*?<\\/a>(*SKIP)(*F)|\\b" . preg_quote($word) . "\\b/";
-			$replacement = '<a href="' . $link . '" data="wordlink">' . ucfirst(preg_quote($word)) . '</a>';
-			$text = preg_replace( $pattern, $replacement, $text );	
-		} 
-		
-        //$text = str_ireplace(array_keys($replace), $replace, $text);
+        // Aggiungi l'elenco delle entità collegate alla fine del contenuto del post
+        $text .= generate_entities_list();
     endif;
+
     return $text;
 }
- 
-add_filter('the_content', 'replace_text_wps');
 
 /* If a post is published the transient must be deleted */
 add_action('publish_post', 'wdlnk_transient_delete');
 function wdlnk_transient_delete($post_id) {
     delete_transient("wdlnk_replace_array");
+}
+
+function generate_entities_list(){
+    global $linked_entities;
+    if(!empty($linked_entities) && (count($linked_entities) < 3) ):
+        $list_html = "<div class=\"post-entities\"><span class=\"post-entities-pre\">In questo articolo:</span><ul>";
+        foreach ($linked_entities as $entity => $link) {
+            $list_html .= "<li><a href='" . esc_url($link) . "'>" . esc_html($entity) . "</a></li>";
+        }
+        $list_html .= "</ul></div>";
+        return $list_html;
+    endif;
+}
+
+add_filter('the_content', 'replace_text_wps', 20);
+
+add_action('wp_head', 'wps_custom_css');
+
+function wps_custom_css(){
+    echo '<style type="text/css">
+        .post-entities {
+            display: flex;
+            font-size: 0.8em;
+        }
+        
+        .post-entities ul {
+            margin-top: 0px;
+            margin-bottom: 0px;
+            list-style: none;
+            padding-left: 1em;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+        }
+    </style>';
 }
